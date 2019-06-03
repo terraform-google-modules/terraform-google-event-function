4abdf02e5440025f0d0c6860fed091f1ba1ff755
Sai Kiran <austin.kiran@gmail.com>
2019-06-03 17:47:10 +0200

    removed unused reference

--- modules/event-project-log-entry/outputs.tf
+++ modules/event-project-log-entry/outputs.tf
@@ -16,5 +16,5 @@
 
 output "function_event_trigger" {
   description = "The information used to trigger the function when a log entry is exported to the topic."
-  value       = "${map("event_type", "google.pubsub.topic.publish", "resource", "${module.destination.resource_name}")}"
+  value       = "${map("event_type", "google.pubsub.topic.publish", "resource", "${google_pubsub_topic.main.name}")}"
 }
