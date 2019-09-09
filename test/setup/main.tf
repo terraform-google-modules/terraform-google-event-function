a45f1d1c0022dc848b726b2e57ebb7bd63f07df1
Andriy Kopachevskyy <kopachevsky@gmail.com>
2019-09-09 13:05:28 +0300

    Event function: update testing to new approach
    To support zone and region params of examples added custom code to main.tf to get random available
    
    Fixes #26

---
+++ test/setup/main.tf
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "random_id" "project" {
+  byte_length = 4
+}
+
+locals {
+  project_name = "ci-event-function-${random_id.project.hex}"
+  region       = random_shuffle.available_regions.result[0]
+  zone         = random_shuffle.available_zones.result[0]
+}
+
+
+module "project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 3.0"
+
+  name                = local.project_name
+  random_project_id   = false
+  auto_create_network = true
+  org_id              = var.org_id
+  folder_id           = var.folder_id
+  billing_account     = var.billing_account
+
+  activate_apis = [
+    "cloudresourcemanager.googleapis.com",
+    "storage-api.googleapis.com",
+    "serviceusage.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "storage-component.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "compute.googleapis.com"
+  ]
+}
+
+data "google_compute_regions" "available" {
+  project = module.project.project_id
+}
+
+resource "random_shuffle" "available_regions" {
+  input        = data.google_compute_regions.available.names
+  result_count = 1
+}
+
+data "google_compute_zones" "available" {
+  project = module.project.project_id
+  region  = local.region
+}
+
+resource "random_shuffle" "available_zones" {
+  input        = data.google_compute_zones.available.names
+  result_count = 3
+}
+
