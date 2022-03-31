9dea6c8501f0d670fbcf387aaffe9415ee1c30bb
bharathkkb <bharathkrishnakb@gmail.com>
2022-03-31 15:43:13 +0000

    keep sa

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -32,11 +32,12 @@ module "project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.0"
 
-  name              = local.project_name
-  random_project_id = true
-  org_id            = var.org_id
-  folder_id         = var.folder_id
-  billing_account   = var.billing_account
+  name                    = local.project_name
+  random_project_id       = true
+  org_id                  = var.org_id
+  folder_id               = var.folder_id
+  billing_account         = var.billing_account
+  default_service_account = "keep"
 
   activate_apis = [
     "cloudresourcemanager.googleapis.com",
