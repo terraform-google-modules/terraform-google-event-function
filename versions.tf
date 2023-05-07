00b6a6f660ca12856da15ad82062a908e3eebcb2
Levente Csőke <32789141+tardigrde@users.noreply.github.com>
2023-05-07 18:58:02 +0200

    Bump google provider min version to v4.23

--- versions.tf
+++ versions.tf
@@ -20,7 +20,7 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.11, < 5.0"
+      version = ">= 4.23, < 5.0"
     }
     null = {
       source  = "hashicorp/null"
